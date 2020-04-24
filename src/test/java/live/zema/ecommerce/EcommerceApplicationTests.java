1c64686fdb64e48a5a6fed2a20b2e6f99b57e7f8
Daniel Niguse <Danielniguse8@gmail.com>
2020-04-24 20:14:25 +0100

    first commit

---
+++ src/test/java/live/zema/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package live.zema.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
