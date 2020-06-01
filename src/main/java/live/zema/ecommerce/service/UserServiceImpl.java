518efdcbfeaaee4b17879e22578513703e8650b2
Daniel Niguse <Danielniguse8@gmail.com>
2020-06-01 20:47:52 +0100

    fixed the deeply nested result of user orders

--- src/main/java/live/zema/ecommerce/service/UserServiceImpl.java
+++ src/main/java/live/zema/ecommerce/service/UserServiceImpl.java
@@ -1,13 +1,19 @@
 package live.zema.ecommerce.service;
 
+import live.zema.ecommerce.domain.LineItem;
+import live.zema.ecommerce.domain.Order;
 import live.zema.ecommerce.domain.User;
 import live.zema.ecommerce.repository.UserRepository;
+import live.zema.ecommerce.web.mapper.DateMapper;
 import live.zema.ecommerce.web.mapper.UserMapper;
+import live.zema.ecommerce.web.model.ItemDto;
+import live.zema.ecommerce.web.model.LineItemDto;
+import live.zema.ecommerce.web.model.OrderDto;
 import live.zema.ecommerce.web.model.UserDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
+import java.util.*;
 
 /**
  * @author danielniguse
@@ -17,17 +23,35 @@ import java.util.Optional;
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
     private final UserMapper userMapper;
+    private final DateMapper dateMapper;
+    private final OrderServiceImpl orderServiceImp;
 
     @Override
     public Optional<UserDto> findByEmail(String email) {
         Optional<User> user = userRepository.findByEmail(email);
-        return user.isEmpty() ? Optional.empty() : Optional.of(userMapper.userToUserDto(user.get()));
+        Set<OrderDto> orders = new HashSet<>();
+        UserDto userDto;
+
+
+        if (user.isEmpty()) {
+            return Optional.empty();
+        }
+
+        for(Order order : user.get().getOrders()){
+            orders.add(new OrderDto(order.getId(),dateMapper.asOffsetDateTime(
+                    order.getCreatedDate()), orderServiceImp.lineItemToLineItemDto(order.getLineItems()),null));
+        }
+
+        userDto = new UserDto(user.get().getEmail(),user.get().getPassword(),
+                null,
+                orders,dateMapper.asOffsetDateTime(user.get().getCreatedDate()));
+
+        return Optional.of(userDto);
     }
 
     @Override
     public UserDto save(UserDto userDto) {
         return userMapper.userToUserDto(userRepository.save(userMapper.userDtoToUser(userDto)));
     }
-
 
 }
