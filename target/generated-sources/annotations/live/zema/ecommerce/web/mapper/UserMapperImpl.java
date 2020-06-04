a074ce3adf33143859d24a286d1877b975808257
Daniel Niguse <Danielniguse8@gmail.com>
2020-06-04 22:08:14 +0100

    update

--- target/generated-sources/annotations/live/zema/ecommerce/web/mapper/UserMapperImpl.java
+++ target/generated-sources/annotations/live/zema/ecommerce/web/mapper/UserMapperImpl.java
@@ -17,7 +17,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2020-06-04T20:43:07+0100",
+    date = "2020-06-04T21:20:58+0100",
     comments = "version: 1.3.1.Final, compiler: javac, environment: Java 11.0.2 (Oracle Corporation)"
 )
 @Component
